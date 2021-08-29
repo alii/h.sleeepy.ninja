ad38ff7d38014314925dde10b5d724272c490212
Alistair Smith <alistairsmith01@gmail.com>
2021-08-29 17:32:58 +0100

    focus search box on render

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,10 +1,18 @@
 import {Engine} from '../types/engine';
 import {useRouter} from 'next/router';
+import {useRef, useEffect} from 'react';
 
 export function Search(props: {engine: Engine}) {
 	const router = useRouter();
+	const ref = useRef<HTMLInputElement>(null);
+
+	useEffect(() => {
+		ref.current?.focus();
+	}, []);
+
 	return (
 		<input
+			ref={ref}
 			className="block row-start-2 py-2 px-5 w-96 bg-gray-50 bg-opacity-20 rounded-md focus:ring ring-white ring-opacity-50 focus:outline-none"
 			placeholder={`Search with ${props.engine.display}...`}
 			onKeyDown={event => {
